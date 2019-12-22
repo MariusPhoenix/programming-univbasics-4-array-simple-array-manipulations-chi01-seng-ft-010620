4145333bd11104a0e5616384710b99e92e5441ef
Marius Phoenix <phoenixzenomarius@gmail.com>
2019-12-22 05:43:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,6 +27,6 @@ end
   end
   
   def using_insert(list_of_programming_languages, python)
-    list_of_programming_languages.insert(pyton)
+    list_of_programming_languages.insert(python)
   end
   
